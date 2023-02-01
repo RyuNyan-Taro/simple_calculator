523cc6e0b48a3a6def9a312bbbf20c99139f38ae
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-02-02 07:47:18 +0900

    in progress GetKey

--- lib/calculation.dart
+++ lib/calculation.dart
@@ -1,9 +1,22 @@
 class Calculator{
-  var number = [];
-  var op = [];
+  static var number = [];
+  static var op = [];
 
+  static String _buffer;
   static void GetKey(String letter){
+    // 四則演算子
+    if(){
 
+    } // C
+    else if(){
+
+    } // =
+    else if(){
+
+    } // number
+    else{
+
+    }
   }
 
   static String Execute(){
