140317f960f2fdcd5d24780e23399a2b05808571
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-02-02 20:05:54 +0900

    add calculation part

--- lib/calculation.dart
+++ lib/calculation.dart
@@ -1,20 +1,27 @@
+const c_op = ['+', '-', 'x', '÷'];
+
 class Calculator{
-  static var number = [];
-  static var op = [];
+  static var _number =  [];
+  static var _op =  [];
 
   static String _buffer;
   static void GetKey(String letter){
     // 四則演算子
-    if(){
-
+    if(c_op.contains(letter)){
+      _op.add(letter);
+      _number.add(double.parse(_buffer));
+      _buffer = '';
     } // C
-    else if(){
-
+    else if(letter == 'C'){
+      _number.clear();
+      _op.clear();
+      _buffer = '';
     } // =
-    else if(){
-
+    else if(letter == '='){
+      return null;
     } // number
     else{
+      _buffer += letter;
 
     }
   }
